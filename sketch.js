a88e6f3c0e46f9de98260dddb65b4e9f5476df86
redpeng-utexas <redpeng@utexas.edu>
2026-03-02 04:58:37 -0600

    Assets

---
+++ sketch.js
@@ -0,0 +1,11 @@
+let table;
+let yisang,faust,donquixote,ryoshu,meursault,honglu,heathcliff,ishmael,rodion,sinclair,gregor;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  table = loadTable('Data/Sinners.csv',',','header');
+}
+
+function draw() {
+  background(220);
+}
